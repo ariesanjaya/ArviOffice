004c0a3aaa057bb0fd3c03f2dbb3d1096886beee
ariesanjaya <sanjaya680@gmail.com>
2020-09-12 12:57:23 +0700

    update verticle

--- webapi/src/main/java/io/arvi/office/database/DatabaseVerticle.java
+++ webapi/src/main/java/io/arvi/office/database/DatabaseVerticle.java
@@ -2,33 +2,13 @@ package io.arvi.office.database;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
 
 public class DatabaseVerticle extends AbstractVerticle {
     
     @Override
     public void start(Promise<Void> promise) throws Exception {
         
-        HttpServer server = vertx.createHttpServer();
-
-        Router router = Router.router(vertx);
-        router.route().handler(BodyHandler.create());
-        router.get("/").handler(context -> {
-            context.response().putHeader("content-type", "text/html")
-            .end("Hello World");
-        });
-
-        server
-            .requestHandler(router)
-            .listen(8080, ar -> {
-                if (ar.succeeded()) {
-                    promise.complete();
-                } else {
-                    promise.fail(ar.cause());
-                }
-            });
+        promise.complete();        
     }
 
 
--- webapi/src/main/java/io/arvi/office/http/HttpServerVerticle.java
+++ webapi/src/main/java/io/arvi/office/http/HttpServerVerticle.java
@@ -2,12 +2,33 @@ package io.arvi.office.http;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class HttpServerVerticle extends AbstractVerticle {
     
     @Override
     public void start(Promise<Void> promise) throws Exception {
         
-        promise.complete();
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        router.get("/").handler(context -> {
+            context.response().putHeader("content-type", "text/html")
+            .end("Hello World");
+        });
+
+        server
+            .requestHandler(router)
+            .listen(8080, ar -> {
+                if (ar.succeeded()) {
+                    promise.complete();
+                } else {
+                    promise.fail(ar.cause());
+                }
+            });
     }
+    
 }
