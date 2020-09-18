ec130365b3b91c11ab3e5143ff2c6ac6b51db0ba
ariesanjaya <sanjaya680@gmail.com>
2020-09-18 07:02:26 +0700

    update journal

--- Postgresql Office Local.session.sql
+++ Postgresql Office Local.session.sql
@@ -50,6 +50,16 @@ DROP TABLE IF EXISTS account_classifications CASCADE;
 DROP TABLE IF EXISTS account_types CASCADE;
 DROP TABLE IF EXISTS accounts CASCADE;
 DROP TYPE IF EXISTS wealth_type;
+DROP TABLE IF EXISTS general_journal CASCADE;
+DROP TABLE IF EXISTS cash_receipt_journal CASCADE;
+DROP TABLE IF EXISTS cash_disbursment_journal CASCADE;
+DROP TABLE IF EXISTS purchase_journal CASCADE;
+DROP TABLE IF EXISTS sales_journal CASCADE;
+DROP TYPE IF EXISTS cash_receipt_debit_type;
+DROP TYPE IF EXISTS cash_receipt_credit_type;
+DROP TYPE IF EXISTS cash_disbursment_debit_type;
+DROP TYPE IF EXISTS cash_disbursment_credit_type;
+DROP TYPE IF EXISTS purchase_debit_type;
 
 CREATE TABLE IF NOT EXISTS kurs(
     kurs_id varchar(48) PRIMARY KEY,
@@ -116,4 +126,129 @@ CREATE TABLE IF NOT EXISTS accounts(
   	deleted boolean not null default false,
     constraint fk_accounts_account_classifications
   		foreign key(class_id) references account_clasifications(class_id)    
+);
+
+CREATE TABLE IF NOT EXISTS general_journal(
+    journal_id varchar(128) PRIMARY KEY,
+    journal_date date NOT NULL,
+    ref_id varchar(128),
+    account_id varchar(128) NOT NULL,
+    description varchar(128),
+    debit_credit varchar(8) NOT NULL,
+    value numeric(18,4),
+
+    version integer not null default 1,
+  	created_date timestamptz not null default CURRENT_TIMESTAMP,
+  	created_by varchar(32),
+  	updated_date timestamptz not null default CURRENT_TIMESTAMP,
+  	updated_by varchar(32),
+  	deleted boolean not null default false
+);
+
+CREATE TYPE cash_receipt_debit_type AS ENUM(
+    'balance',
+    'sales_discount'
+);
+
+CREATE TYPE cash_receipt_credit_type AS ENUM(
+    'account_receivable',
+    'sales',
+    'other'
+);
+
+CREATE TYPE cash_disbursment_debit_type AS ENUM(
+    'balance',
+    'purchase_discount'
+);
+
+CREATE TYPE cash_disbursment_credit_type AS ENUM(
+    'account_receivable',
+    'sales',
+    'other'
+);
+
+-- Jurnal Penerimaan Kas
+CREATE TABLE IF NOT EXISTS cash_receipt_journal(
+    journal_id varchar(128) PRIMARY KEY,
+    journal_date date NOT NULL,
+    posted boolean DEFAULT false,
+    account_id varchar(128) NOT NULL,
+    description varchar(128),
+    debit_credit varchar(8),
+    debit_type cash_receipt_debit_type,
+    credit_type cash_receipt_credit_type,
+    other_ref_id varchar(128),
+    other_account_id varchar(128),
+    value numeric(18,4),
+
+    version integer not null default 1,
+  	created_date timestamptz not null default CURRENT_TIMESTAMP,
+  	created_by varchar(32),
+  	updated_date timestamptz not null default CURRENT_TIMESTAMP,
+  	updated_by varchar(32),
+  	deleted boolean not null default false
+);
+
+-- Jurnal Pengeluaran Kas
+CREATE TABLE IF NOT EXISTS cash_disbursment_journal(
+    journal_id varchar(128) PRIMARY KEY,
+    journal_date date NOT NULL,
+    posted boolean DEFAULT false,
+    account_id varchar(128) NOT NULL,
+    description varchar(128),
+    debit_credit varchar(8),
+    debit_type cash_receipt_debit_type,
+    credit_type cash_receipt_credit_type,
+    other_ref_id varchar(128),
+    other_account_id varchar(128),
+    value numeric(18,4),
+
+    version integer not null default 1,
+  	created_date timestamptz not null default CURRENT_TIMESTAMP,
+  	created_by varchar(32),
+  	updated_date timestamptz not null default CURRENT_TIMESTAMP,
+  	updated_by varchar(32),
+  	deleted boolean not null default false
+);
+
+CREATE TYPE purchase_debit_type AS ENUM(
+    'purchase',
+    'other'
+);
+
+CREATE TABLE IF NOT EXISTS purchase_journal(
+    journal_id varchar(128) PRIMARY KEY,
+    journal_date date NOT NULL,
+    posted boolean DEFAULT false,
+    account_id varchar(128) NOT NULL,
+    description varchar(128),
+    debit_credit varchar(8),
+    debit_type purchase_debit_type,
+    other_ref_id varchar(128),
+    other_account_id varchar(128),
+    value numeric(18,4),
+
+    version integer not null default 1,
+  	created_date timestamptz not null default CURRENT_TIMESTAMP,
+  	created_by varchar(32),
+  	updated_date timestamptz not null default CURRENT_TIMESTAMP,
+  	updated_by varchar(32),
+  	deleted boolean not null default false 
+);
+
+CREATE TABLE IF NOT EXISTS sales_journal(
+    journal_id varchar(128) PRIMARY KEY,
+    journal_date date NOT NULL,
+    posted boolean DEFAULT false,
+    account_id varchar(128) NOT NULL,
+    description varchar(128),
+    debt_range varchar(64),
+    value numeric(18,4),
+
+    version integer not null default 1,
+  	created_date timestamptz not null default CURRENT_TIMESTAMP,
+  	created_by varchar(32),
+  	updated_date timestamptz not null default CURRENT_TIMESTAMP,
+  	updated_by varchar(32),
+  	deleted boolean not null default false 
 );
